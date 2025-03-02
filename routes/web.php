8041e9b48ec5c11341c92c0eb59fa84fa0840761
nelsonbarragan07 <nelsondavidbarragandehoyos@gmail.com>
2025-03-02 17:52:00 -0500

    segundo commit

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EstudianteController;
 
-Route::resource('estudiantes', EstudianteController::class);
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('estudiantes', EstudianteController::class);
